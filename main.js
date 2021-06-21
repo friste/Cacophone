6d51819a7da281ca4584266ddd827c27053333e3
fristefristefriste <83371646+fristefristefriste@users.noreply.github.com>
2021-06-20 21:00:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-let xp, yp;
+let xp, yp, toggle;
+let DRAG, TAP, GAZE, val;
+DRAG = 1;
+TAP = 2;
+GAZE = 3;
 
-///////////////////////////////////////
+
 class Voice {
   constructor(frequency, shape) {
     this.synth = new p5.Oscillator();
@@ -18,7 +22,6 @@ class Voice {
     this.synth.connect(rev1);
     rev1.disconnect();
     rev1.connect(lp1);
-    // rev1.process(this.synth, 6, 2);
   }
   
   setFreq(f) {
@@ -30,15 +33,61 @@ class Voice {
   }
 }
 
+
+class Ambience {
+  constructor() {
+    
+  }
+}
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(220);
   
-  v1 = new Voice(200, 'sine');
-  v2 = new Voice(400, 'sawtooth');
-  v3 = new Voice(100, 'sawtooth');
+  toggle = createSlider(1, 3, 1);
+  
+  v1 = new Voice(200, 'triangle');
+  v2 = new Voice(300, 'sine');
+  v3 = new Voice(100, 'triangle');
   frameRate(100);
+  
+}
 
+function draw() {
+  
+  val = toggle.value();
+  
+  if (val == 1) {
+    drag();
+  }
+  else if (val == 2) {
+    tap();
+  }
+  else {
+    gaze();
+  }
+  
+  r = map(mouseX, 0, windowWidth, 10, 20);
+  g = map(mouseY, 0, windowHeight, 1, 30);
+  b = map(mouseX, 0, windowWidth, 1, 30);
+  background(r, g, b);
+  
+}
+
+function drag() {
+  if (mouseIsPressed) {
+    freq1 = constrain(map(mouseX, 0, width, 50, 700), 50, 700);
+    v1.setFreq(freq1);
+    v1.sound();
+  }
+}
+
+function tap() {
+  
+}
+
+function gaze() {
   webgazer.setGazeListener(function(data, elapsedTime) {
     if (data == null) {
         return;
@@ -46,22 +95,7 @@ function setup() {
      xp = data.x;
      yp = data.y;
 
-    //elapsed time is based on time since begin was called
   }).begin();
 }
 
-function draw() {
-  // if (mouseIsPressed) {
-  var round = Math.floor(xp);
-    freq1 = constrain(map(round, 0, width, 50, 700), 50,       700);
-    // freq2 = constrain(map(round, 0, width, 100, 400), 100,       400);
-    // freq3 = constrain(map(round, 0, width, 200, 400), 200,       400);
-    v1.setFreq(freq1);
-    v1.sound();
-    // v2.setFreq(freq2);
-    // v2.sound();
-    // v2.setFreq(freq3);
-    // v3.sound();
-  // }
-}
-///////////////////////////////////////
+/////////////
